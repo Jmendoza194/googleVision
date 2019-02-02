4231d75dd1e7d9e779a2613f9f7d52175d560c68
Jose Mendoza <joseemendoza194@gmail.com>
2019-02-02 00:49:15 -0800

    add api capability

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import vision from '../apis/googleVision';
+import Pic from './Pic.jpg';
+import Axios from 'axios';
+
+
+const req =
+    {
+        "requests":[
+          {
+            "image":{
+              "source":{
+                "imageUri":
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/CarsonWentz11.jpg/220px-CarsonWentz11.jpg"
+              }
+            },
+            "features":[
+              {
+                "type":"LABEL_DETECTION",
+                "maxResults":1
+              }
+            ]
+          }
+        ]
+      }
+
+Axios.post('https://vision.googleapis.com/v1/images:annotate?key=', req).then((data) => {
+    console.log(data)
+}).catch((error) => {
+    console.log(error);
+})  
 
 const App = () =>{
+    console.log(req)
 return <div>App</div>;
 }
+
 
 export default App;
